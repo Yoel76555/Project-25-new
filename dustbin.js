060063d397bd6ed61efd9d9fdbe35369850e8577
Yoel76555 <71801083+Yoel76555@users.noreply.github.com>
2020-10-26 23:45:11 -0600

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,41 @@
+class Dustbin {
+    constructor(x,y,width,height){
+         this.x=x;
+         this.y=y;
+         this.width=width;
+         this.height=height;
+         
+         this.rightBody=Bodies.rectangle(710,640,20,100,{isStatic:true});
+         World.add(world,this.rightBody);
+
+         this.leftBody=Bodies.rectangle(840,640,20,100,{isStatic:true});
+         World.add(world,this.leftBody);
+
+         this.bottomBody=Bodies.rectangle(775,640,110,20,{isStatic:true});
+         World.add(world,this.bottomBody);
+
+         this.image=loadImage("dustbingreen.png");
+
+         
+    }
+    display(){
+        push();
+        translate(this.rightBody.position.x,this.rightBody.position.y);
+        rectMode(CENTER);
+        pop();
+        
+        push();
+        translate(this.leftBody.position.x,this.leftBody.position.y);
+        rectMode(CENTER);
+        pop();
+
+        push();
+        translate(this.bottomBody.position.x,this.bottomBody.position.y);
+        rectMode(CENTER);
+        
+        imageMode(CENTER);
+        image(this.image, 0,0,this.width-30, this.height-50);
+        pop();
+        }
+
+}
